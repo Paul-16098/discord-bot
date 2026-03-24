a59124459d4d8cd85917491048dbb96d061aec99
Paul-16098 <pl816098@gmail.com>
2026-03-24 11:31:04 +0800

    style: use tab

--- src/main.rs
+++ src/main.rs
@@ -13,70 +13,70 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 pub struct Data {}
 
 async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
-    // This is our custom error handler
-    // They are many errors that can occur, so we only handle the ones we want to customize
-    // and forward the rest to the default handler
-    match error {
-        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
-        poise::FrameworkError::Command { error, ctx, .. } => {
-            error!("Error in command `{}`: {:?}", ctx.command().name, error);
-        }
-        error => {
-            if let Err(e) = poise::builtins::on_error(error).await {
-                error!("Error while handling error: {}", e)
-            }
-        }
-    }
+	// This is our custom error handler
+	// They are many errors that can occur, so we only handle the ones we want to customize
+	// and forward the rest to the default handler
+	match error {
+		poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+		poise::FrameworkError::Command { error, ctx, .. } => {
+			error!("Error in command `{}`: {:?}", ctx.command().name, error);
+		}
+		error => {
+			if let Err(e) = poise::builtins::on_error(error).await {
+				error!("Error while handling error: {}", e)
+			}
+		}
+	}
 }
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+	tracing_subscriber::fmt::init();
 
-    let framework = poise::Framework::builder()
-        .setup(move |ctx, ready, framework| {
-            Box::pin(async move {
-                info!("Logged in as {}", ready.user.name);
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
-            })
-        })
-        .options(poise::FrameworkOptions {
-            commands: vec![
-                commands::help(),
-                commands::ping(),
-                commands::clear(),
-                commands::nuke(),
-            ],
-            prefix_options: Default::default(),
-            // The global error handler for all error cases that may occur
-            on_error: |error| Box::pin(on_error(error)),
-            // This code is run before every command
-            pre_command: |ctx| {
-                Box::pin(async move {
-                    debug!("Executing command {}...", ctx.command().qualified_name);
-                })
-            },
-            // This code is run after a command if it was successful (returned Ok)
-            post_command: |ctx| {
-                Box::pin(async move {
-                    debug!("Executed command {}!", ctx.command().qualified_name);
-                })
-            },
-            // Every command invocation must pass this check to continue execution
-            command_check: Some(|_ctx| Box::pin(async move { Ok(true) })),
-            ..Default::default()
-        })
-        .build();
+	let framework = poise::Framework::builder()
+		.setup(move |ctx, ready, framework| {
+			Box::pin(async move {
+				info!("Logged in as {}", ready.user.name);
+				poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+				Ok(Data {})
+			})
+		})
+		.options(poise::FrameworkOptions {
+			commands: vec![
+				commands::help(),
+				commands::ping(),
+				commands::clear(),
+				commands::nuke(),
+			],
+			prefix_options: Default::default(),
+			// The global error handler for all error cases that may occur
+			on_error: |error| Box::pin(on_error(error)),
+			// This code is run before every command
+			pre_command: |ctx| {
+				Box::pin(async move {
+					debug!("Executing command {}...", ctx.command().qualified_name);
+				})
+			},
+			// This code is run after a command if it was successful (returned Ok)
+			post_command: |ctx| {
+				Box::pin(async move {
+					debug!("Executed command {}!", ctx.command().qualified_name);
+				})
+			},
+			// Every command invocation must pass this check to continue execution
+			command_check: Some(|_ctx| Box::pin(async move { Ok(true) })),
+			..Default::default()
+		})
+		.build();
 
-    let token = var("DISCORD_TOKEN")
-        .expect("Missing `DISCORD_TOKEN` env var, see README for more information.");
+	let token = var("DISCORD_TOKEN")
+		.expect("Missing `DISCORD_TOKEN` env var, see README for more information.");
 
-    let intents = serenity::GatewayIntents::non_privileged();
+	let intents = serenity::GatewayIntents::non_privileged();
 
-    let client = serenity::ClientBuilder::new(token, intents)
-        .framework(framework)
-        .await;
+	let client = serenity::ClientBuilder::new(token, intents)
+		.framework(framework)
+		.await;
 
-    client.unwrap().start().await.unwrap()
+	client.unwrap().start().await.unwrap()
 }
